83af256636d4862e11bc90f13a014adbe503b141
Michael Gilliland <mjg.py3@gmail.com>
2016-07-02 13:34:25 -0400

    Allow specification of "which" note. I'm thinking middle C can be C 0

--- src/music.fsx
+++ src/music.fsx
@@ -1,11 +1,11 @@
 type NaturalPitch =
-  | C
-  | D
-  | E
-  | F
-  | G
-  | A
-  | B
+  | C of int
+  | D of int
+  | E of int
+  | F of int
+  | G of int
+  | A of int
+  | B of int
 
 type AccidentalPitch =
   | Sharp of NaturalPitch
@@ -18,32 +18,32 @@ type Note =
 type Direction = | Up | Down
 
 let semitone direction note = match direction, note with
-  | Up, Natural C -> Sharp C |> Accidental
-  | Up, Accidental (Sharp C) -> Natural D
-  | Up, Natural D -> Sharp D |> Accidental
-  | Up, Accidental (Sharp D) -> Natural E
-  | Up, Natural E -> Natural F
-  | Up, Natural F -> Sharp F |> Accidental
-  | Up, Accidental (Sharp F) -> Natural G
-  | Up, Natural G -> Sharp G |> Accidental
-  | Up, Accidental (Sharp G) -> Natural A
-  | Up, Natural A -> Sharp A |> Accidental
-  | Up, Accidental (Sharp A) -> Natural B
-  | Up, Natural B -> Natural C
+  | Up, Natural (C n) -> C n |> Sharp |> Accidental
+  | Up, Accidental (Sharp (C n)) -> D n |> Natural
+  | Up, Natural (D n) -> D n |> Sharp |> Accidental
+  | Up, Accidental (Sharp (D n)) -> E n |> Natural
+  | Up, Natural (E n) -> F n |> Natural
+  | Up, Natural (F n) -> F n |> Sharp |> Accidental
+  | Up, Accidental (Sharp (F n)) -> G n |> Natural
+  | Up, Natural (G n) -> G n |> Sharp |> Accidental
+  | Up, Accidental (Sharp (G n)) -> A n |> Natural
+  | Up, Natural (A n) -> A n |> Sharp |> Accidental
+  | Up, Accidental (Sharp (A n)) -> B n |> Natural
+  | Up, Natural (B n) -> n + 1 |> C |> Natural
 
-  | Down, Accidental (Sharp C) -> Natural C
-  | Down, Natural D -> Accidental (Sharp C)
-  | Down, Accidental (Sharp D) -> Natural D
-  | Down, Natural E -> Accidental (Sharp D)
-  | Down, Natural F -> Natural E
-  | Down, Accidental (Sharp F) -> Natural F
-  | Down, Natural G -> Accidental (Sharp F)
-  | Down, Accidental (Sharp G) -> Natural G
-  | Down, Natural A -> Accidental (Sharp G)
-  | Down, Accidental (Sharp A) -> Natural A
-  | Down, Natural B -> Accidental (Sharp A)
-  | Down, Natural C -> Natural B
-  | n -> failwith "Not a real note %A" n
+  | Down, Accidental (Sharp (C n)) -> C n |> Natural
+  | Down, Natural (D n) -> C n |> Sharp |> Accidental
+  | Down, Accidental (Sharp (D n)) -> D n |> Natural
+  | Down, Natural (E n) -> D n |> Sharp |> Accidental
+  | Down, Natural (F n) -> E n |> Natural
+  | Down, Accidental (Sharp (F n)) -> F n |> Natural
+  | Down, Natural (G n) -> F n |> Sharp |> Accidental
+  | Down, Accidental (Sharp (G n)) -> G n |> Natural
+  | Down, Natural (A n) ->  G n |> Sharp |> Accidental
+  | Down, Accidental (Sharp (A n)) -> A n |> Natural
+  | Down, Natural (B n) -> A n |> Sharp |> Accidental
+  | Down, Natural (C n) -> n - 1 |> B |> Natural
+  | n -> failwithf "Not a real note %A" n
 
 let rec transpose direction amount note = match direction, amount with
   | _, 0 -> note
@@ -54,5 +54,5 @@ let tone direction = transpose direction 2
 
 [<EntryPoint>]
 let main args =
-  printfn "Arguments passed to function : %A" args
+  printfn "%A" <| Seq.map (tone Up) [A 0 |> Natural; B 0 |> Natural]
   0
