ece6e668fe24cd81d1b4c60056c697129f931cfd
Michael "Gilli" Gilliland <mjg.py3@gmail.com>
2020-10-20 13:46:38 -0400

    feat: add a few scales

--- westmusic.ml
+++ westmusic.ml
@@ -42,22 +42,59 @@ let next_lower_note_name = function
 
 type nat = | Zero | Succ of nat;;
 
-type note = { name : note_name; octave: int };;
+let n1 = Succ Zero;;
+let n2 = Succ n1;;
+let n3 = Succ n2;;
+let n4 = Succ n3;;
+let n5 = Succ n4;;
+let n6 = Succ n5;;
+let n7 = Succ n6;;
+let n8 = Succ n7;;
+let n9 = Succ n8;;
+let n10 = Succ n9;;
+let n11 = Succ n10;;
+let n12 = Succ n11;;
 
-type chord =
-  | Notes of note list;;
+let rec n_times v f = function
+  | Zero -> v
+  | Succ k -> n_times (f v) f k;;
+
+type note = { name : note_name; octave: int };;
 
 type pitch_direction = | Higher | Lower;;
 
-let semitone ({ name; octave } as note) dir =
+let (<<<) f g x = f (g x);;
+
+let semitone dir ({ name; octave } as note) =
   match name, dir with
     | B, Higher -> { name=next_higher_note_name name; octave=octave+1 }
     | _, Higher -> { note with name=next_higher_note_name name }
     | C, Lower -> { name=next_lower_note_name name; octave=octave-1 }
     | _, Lower -> { note with name=next_lower_note_name name };;
 
-let rec transpose note dir = function
-  | Zero -> note
-  | Succ k -> transpose (semitone note dir) dir k;;
+let tone dir = semitone dir <<< semitone dir;;
+
+let transpose_note dir note = n_times note (semitone dir);;
+
+let scale_from_offsets offsets tonic =
+  List.map (transpose_note Higher tonic) offsets;;
+
+let natural_major_offset =
+  [ Zero; n2; n4; n5; n7; n9; n11; n12 ];;
+
+let natural_major_scale_from_tonic =
+  scale_from_offsets natural_major_offset;;
+
+let natural_minor_offset =
+  [ Zero; n2; n3; n5; n7; n8; n10; n12 ];;
+
+let natural_minor_scale_from_tonic =
+  scale_from_offsets natural_minor_offset;;
+
+let blues_offset =
+  [Zero; n3; n5; n6; n7; n10; n12];;
+
+let blues_scale_from_tonic =
+  scale_from_offsets natural_minor_offset;;
 
 let () = print_endline "Hello, World!"
